8b3813a4659811e9e0549e7d563e79184c156503
William Butler <cybersentinel615@gmail.com>
2025-11-02 17:16:54 -0600

    Create WN11-CC-000005.ps1

---
+++ STIGS/WN11-CC-000005.ps1
@@ -0,0 +1,75 @@
+<# 
+
+.SYNOPSIS 
+
+    This PowerShell script ensures camera access from the lock screen is disabled by setting the NoLockScreenCamera registry value to 1 
+
+  
+
+.NOTES 
+
+    Author          : William Butler 
+
+    LinkedIn        : linkedin.com/in/williambutlercybersentinel 
+
+    GitHub          : https://github.com/Will-B615 
+
+    Date Created    : 2025-11-02 
+
+    Last Modified   : 2025-11-02 
+
+    Version         : 1.0 
+
+    CVEs            : N/A 
+
+    Plugin IDs      : N/A 
+
+    STIG-ID         : WN11-CC-000005 
+
+  
+
+.TESTED ON 
+
+    Date(s) Tested  :  
+
+    Tested By       :  
+
+    Systems Tested  :  
+
+    PowerShell Ver. :  
+
+  
+
+.USAGE 
+
+    Put any usage instructions here. 
+
+    Example syntax: 
+
+    PS C:\> .\__remediation_template(STIG-ID-WN11-CC-000005).ps1  
+
+#> 
+
+$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization" 
+
+$propertyName = "NoLockScreenCamera" 
+
+$propertyValue = 1 
+
+  
+
+# Create the registry key if it doesn't exist 
+
+if (-not (Test-Path $registryPath)) { 
+
+    New-Item -Path $registryPath -Force 
+
+} 
+
+  
+
+# Disable lock screen camera access 
+
+Set-ItemProperty -Path $registryPath -Name $propertyName -Value $propertyValue -Type DWord 
+
+ 
