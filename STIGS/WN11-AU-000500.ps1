a339ecfb1bb8983f21dbb831c61e22530f7d9d9b
William Butler <cybersentinel615@gmail.com>
2025-11-02 09:51:24 -0600

    Create WN11-AU-000500.ps1

---
+++ STIGS/WN11-AU-000500.ps1
@@ -0,0 +1,65 @@
+ 
+
+    This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB). 
+
+  
+
+.NOTES 
+
+    Author          : William Butler 
+
+    LinkedIn        : linkedin.com/in/williambutlercybersentinel 
+
+    GitHub          : https://github.com/Will-B615 
+
+    Date Created    : 2025-11-02 
+
+    Last Modified   : 2025-11-02 
+
+    Version         : 1.0 
+
+    CVEs            : N/A 
+
+    Plugin IDs      : N/A 
+
+    STIG-ID         : WN11-AU-000500 
+
+  
+
+.TESTED ON 
+
+    Date(s) Tested  :  
+
+    Tested By       :  
+
+    Systems Tested  :  
+
+    PowerShell Ver. :  
+
+  
+
+.USAGE 
+
+    Put any usage instructions here. 
+
+    Example syntax: 
+
+    PS C:\> .\__remediation_template(STIG-ID-WN11-AU-000500).ps1  
+
+#> 
+
+  
+
+# YOUR CODE GOES HERE 
+
+$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\Application"
+$propertyName = "MaxSize"
+$propertyValue = 32768
+
+# Create the registry key if it doesn't exist
+if (-not (Test-Path $registryPath)) {
+    New-Item -Path $registryPath -Force
+}
+
+# Set the Maximum Log Size to 32768 KB
+Set-ItemProperty -Path $registryPath -Name $propertyName -Value $propertyValue -Type DWord
