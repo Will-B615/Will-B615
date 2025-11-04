5f636a4b83940425d3cb8fed35b01c9b284a41e1
William Butler <cybersentinel615@gmail.com>
2025-11-04 11:31:51 -0600

    Create WN11-CC-000327.ps1

---
+++ STIGS/WN11-CC-000327.ps1
@@ -0,0 +1,73 @@
+<# 
+
+.SYNOPSIS 
+
+    This PowerShell script enables PowerShell transcription by setting the EnableTranscripting registry value to 1. 
+
+  
+
+.NOTES 
+
+    Author          : William Butler 
+
+    LinkedIn        : linkedin.com/in/williambutlercybersentinel 
+
+    GitHub          : https://github.com/Will-B615 
+
+    Date Created    : 2025-11-02 
+
+    Last Modified   : 2025-11-02 
+
+    Version         : 1.0 
+
+    CVEs            : N/A 
+
+    Plugin IDs      : N/A 
+
+    STIG-ID         : WN11-CC-000327 
+
+  
+
+.TESTED ON 
+
+    Date(s) Tested  :  
+
+    Tested By       :  
+
+    Systems Tested  :  
+
+    PowerShell Ver. :  
+
+  
+
+.USAGE 
+
+    Put any usage instructions here. 
+
+    Example syntax: 
+
+    PS C:\> .\__remediation_template(STIG-ID-WN11-CC-000327).ps1  
+
+#> 
+
+$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\PowerShell\Transcription" 
+
+$propertyName = "EnableTranscripting" 
+
+$propertyValue = 1 
+
+  
+
+# Create the registry key if it doesn't exist 
+
+if (-not (Test-Path $registryPath)) { 
+
+    New-Item -Path $registryPath -Force 
+
+} 
+
+  
+
+# Enable PowerShell transcription 
+
+Set-ItemProperty -Path $registryPath -Name $propertyName -Value $propertyValue -Type DWord 
