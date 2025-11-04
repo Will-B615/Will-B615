d8cb675bcc7ce0d3001e01286b9fb8ffbca1169e
William Butler <cybersentinel615@gmail.com>
2025-11-04 11:41:15 -0600

    Create WN11-AU-000510.ps1

---
+++ STIGS/WN11-AU-000510.ps1
@@ -0,0 +1,73 @@
+<# 
+
+.SYNOPSIS 
+
+    This PowerShell script sets the maximum system event log file size to 32,768 KB by configuring the MaxSize registry value. 
+
+  
+
+.NOTES 
+
+    Author          : William Butler 
+
+    LinkedIn        : linkedin.com/in/williambutlercybersentinel 
+
+    GitHub          : https://github.com/Will-B615 
+
+    Date Created    : 2025-11-02 
+
+    Last Modified   : 2025-11-02 
+
+    Version         : 1.0 
+
+    CVEs            : N/A 
+
+    Plugin IDs      : N/A 
+
+    STIG-ID         : WN11-AU-000510 
+
+  
+
+.TESTED ON 
+
+    Date(s) Tested  :  
+
+    Tested By       :  
+
+    Systems Tested  :  
+
+    PowerShell Ver. :  
+
+  
+
+.USAGE 
+
+    Put any usage instructions here. 
+
+    Example syntax: 
+
+    PS C:\> .\__remediation_template(STIG-ID-WN11-AU-000510).ps1  
+
+#> 
+
+$registryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\System" 
+
+$propertyName = "MaxSize" 
+
+$propertyValue = 32768 
+
+  
+
+# Create the registry key if it doesn't exist 
+
+if (-not (Test-Path $registryPath)) { 
+
+    New-Item -Path $registryPath -Force 
+
+} 
+
+  
+
+# Set System Event Log maximum size 
+
+Set-ItemProperty -Path $registryPath -Name $propertyName -Value $propertyValue -Type DWord 
